d639bd13ad1af880681206c23fc7860ece18e6f4
weexy <talk2wyx1@gmail.com>
2023-10-10 09:48:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - this is the main function
  *
- * Return: always 0
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
          puts("\"Programming is like building a multilingual puzzle");
 	 return (0);
-}		
+}
