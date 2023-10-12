9b27f9b57a5de992d1fd8003bc041d8a7a2db92a
weexy <talk2wyx1@gmail.com>
2023-10-12 15:01:01 -0700

    largest number

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -7,17 +7,17 @@
 */
 
 int main(void)
-'{
-	int "a, b, c;"
+{
+	int a, b, c;
 	int largest;
 
 	a = 972;
 	b = -98;
 	c = 0;
 
-	largest = "largest_number(a, b, c)";
+	largest = largest_number(a, b, c);
 
-	printf("%d is the largest number\n", largest));
+	printf("%d is the largest number\n", largest);
 
 	return (0);
-}'
+}
