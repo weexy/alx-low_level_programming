e9f6b58657f1e90439f7172cd454a35128ec88e9
weexy <talk2wyx1@gmail.com>
2023-10-17 08:40:33 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,12 @@
 
 /**
 * main - function that prints _putchar
-* Return: returns 0;
+* Return: returns 0';'
 */
 
 int main(void)
 {
-	_putchar('_');
+	_putchar('c');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -16,6 +16,6 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return (0);
+	(return (0)';');
 }
 
