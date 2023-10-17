30467390cc1cc33798ecc8559b153f3be596e0ac
weexy <talk2wyx1@gmail.com>
2023-10-17 06:33:37 -0700

    101 keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,17 +8,18 @@
 *
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	int pass[100];
 	int i, sum, n;
 
-	sum = 0;    
+	sum = 0;
 
-		srand(time(NULL));
+	srand(time(NULL));
 
-		for (i = 0; i < 100; i++)
-	{	
+	for (i = '0'; i < 100; i++)
+	{
 	pass[i] = rand() % 78;
 	sum += (pass[i] + '0');
 	putchar(pass[i] + '0');
