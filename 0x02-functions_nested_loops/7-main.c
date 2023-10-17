73b014e49728f93a2ceea88f18912122dfb684f0
weexy <talk2wyx1@gmail.com>
2023-10-17 11:19:59 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,27 +1,18 @@
 #include "main.h"
 
-
 /**
- * print_last_digit - entry point
- *
- * Description: function that prints the last digit of a number.
- *
- * @a:number to compute last digit
- *
- * Return: last digit
- */
-
-
-int print_last_digit(int a)
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
 {
-	int last_digit;
+    int r;
 
-
-	last_digit = a % 10;
-	if (last_digit < 0)
-	{
-		last_digit = last_digit * -1;
-	}
-	_putchar(last_digit + '0');
-	return (last_digit);
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
 }
