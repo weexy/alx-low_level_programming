7f511ef947a6e6a5b9657fd6016e91267882a4f2
weexy <talk2wyx1@gmail.com>
2023-10-21 09:24:52 -0700

    101 print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,27 +2,26 @@
 
 /**
  * print_number - main function
- *
- * @n: Function parameter
- *
- * Return: Always 0.
+ * @n: function parameter
+ * Return: Always 0
  */
-
 void print_number(int n)
 {
 	unsigned int n1;
 
-	n1 = n;
-
 	if (n < 0)
 	{
-	_putchar('-');
-	n1 = -n;
+		_putchar('-');
+		n1 = -n;
+	}
+	else
+	{
+		n1 = n;
 	}
 
 	if (n1 / 10 != 0)
 	{
-	print_number(n1 / 10);
+		print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
 }
