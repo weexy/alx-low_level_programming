103c12d0b61a44cbe1685c2e0d18e309e724fbc8
weexy <talk2wyx1@gmail.com>
2023-11-01 09:27:28 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,38 +2,28 @@
 #include <stdlib.h>
 
 /**
-* alloc_grid - 2 dimensional array
-* @width: number of columns
-* @height: number of rows
-* Return: pointer to array
+* main - multiplies two numbers
+* @argc: arguments count
+* @argv: arguments vector
+* Return: 0
 */
 
-int **alloc_grid(int width, int height)
+int main(int argc, char *argv[])
 {
-	int i, j;
-	int **output;
+	int num1, num2, mul = 0;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	output = malloc(sizeof(int *) * height);
-
-	if (output == NULL)
-		return (NULL);
-
-	for (i = 0; i < height; i++)
+	if (argc != 3)
 	{
-		output[i] = malloc(sizeof(int) * width);
-
-		if (output[i] == NULL)
-		{
-			free(output);
-			for (j = 0; j <= height; j++)
-				free(output[j]);
-			return (NULL);
-		}
-		for (j = 0; j < width; j++)
-			output[i][j] = 0;
+		printf("Error\n");
+		return (1);
 	}
-	return (output);
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+
+		mul = num1 * num2;
+		printf("%d\n", mul);
+	}
+	return (0);
 }
