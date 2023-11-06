1c1018e1830beea98a1175de27d7e6c90e691a7d
weexy <talk2wyx1@gmail.com>
2023-11-06 08:40:10 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+
 int main(void)
 {
-	printf("%s\n", __TIME__);
-	return(0);
+	printf("%s\n", __FILE__);
+
+	return (0);
 }
---
+++ 0x0D-preprocessor/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", __TIME__);
+}
